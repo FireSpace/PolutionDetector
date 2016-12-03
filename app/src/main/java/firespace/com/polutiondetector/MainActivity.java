6369a934a4476bca5e82ecae2a6b03c2b7dc7a6f
shrey <shreeachari@gmail.com>
2016-12-03 15:16:15 -0500

    putting new stuff

--- app/src/main/java/firespace/com/polutiondetector/MainActivity.java
+++ app/src/main/java/firespace/com/polutiondetector/MainActivity.java
@@ -1,39 +1,64 @@
 package firespace.com.polutiondetector;
 
+
 import android.content.Context;
+import android.content.Intent;
 import android.hardware.Sensor;
 import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 
 import java.util.ArrayList;
 import java.util.Collections;
-
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity implements SensorEventListener {
 
+    Button settingsButton;
     private SensorManager sensorManager;
     private Sensor temperature;
     private Sensor humidity;
     private Sensor pressure;
     private int listLength = 30;
-    double constant = 216.7;
-    public ArrayList<Double> list = new ArrayList<Double>();
-    public ArrayList<Double> listH = new ArrayList<Double>();
-
+    public ArrayList<Double> list = new ArrayList();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);// Get an instance of the sensor service, and use that to get an instance of
-        // a particular sensor.
+        setContentView(R.layout.activity_main);
+        settingsButton = (Button) findViewById(R.id.settings);
         sensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
         temperature = sensorManager.getDefaultSensor(Sensor.TYPE_AMBIENT_TEMPERATURE);
         humidity = sensorManager.getDefaultSensor(Sensor.TYPE_RELATIVE_HUMIDITY);
         pressure = sensorManager.getDefaultSensor(Sensor.TYPE_PRESSURE);
+        if (sensorManager.getDefaultSensor(Sensor.TYPE_PRESSURE) != null) {
+            List<Sensor> pressure = mSensorManager.getSensorList(Sensor.TYPE_PRESSURE);
+            System.out.println("There is air pressure");
+        } else {
+            System.out.println("There is no gyroscope");
+        }
+
+
+        if (mSensorManager.getDefaultSensor(Sensor.TYPE_RELATIVE_HUMIDITY) != null) {
+            List<Sensor> pressure = mSensorManager.getSensorList(Sensor.TYPE_RELATIVE_HUMIDITY);
+            System.out.println("There is a HUMIDITY");
+        } else {
+            System.out.println("There is no HUMIDITY");
+        }
+
+
+        settingsButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(MainActivity.this, Settings.class));
+            }
+        });
     }
+
 
     @Override
     public void onSensorChanged(SensorEvent sensorEvent) {
@@ -60,6 +85,14 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
                 System.out.println("The max is: " + Collections.min(list) + "The min is: " + Collections.min(list));
             }
         }
+
+        if (millibars_of_pressure >= Collections.min(list) && millibars_of_pressure <= Collections.min(list)) {
+            System.out.println("Your ambient air pressure is acceptable");
+        }
+
+        else if(millibars_of_pressure <= Collections.min(list) && millibars_of_pressure >= Collections.min(list)){
+            System.out.println("Your ambient air pressure is too high. There may be dangerous levels of pollutants");
+        }
     }
 
     @Override
@@ -69,18 +102,15 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
 
     @Override
     protected void onResume() {
-        // Register a listener for the sensor.
         super.onResume();
         sensorManager.registerListener(this, humidity, SensorManager.SENSOR_DELAY_NORMAL);
         sensorManager.registerListener(this, temperature, SensorManager.SENSOR_DELAY_NORMAL);
         sensorManager.registerListener(this, pressure, SensorManager.SENSOR_DELAY_NORMAL);
     }
 
-
     @Override
     protected void onPause() {
-        // Be sure to unregister the sensor when the activity pauses.
         super.onPause();
-        sensorManager.unregisterListener(this);
+        mSensorManager.unregisterListener(this);
     }
 }
