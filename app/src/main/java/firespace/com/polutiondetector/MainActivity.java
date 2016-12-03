aaa94afbc08a888f829b9ab564db63e8a3c99c2b
Kaushik <kaushikpprakash@gmail.com>
2016-12-03 15:08:51 -0500

    Working Min and Max range of the barometer readings and Other sensors added

--- app/src/main/java/firespace/com/polutiondetector/MainActivity.java
+++ app/src/main/java/firespace/com/polutiondetector/MainActivity.java
@@ -10,65 +10,77 @@ import android.support.v7.app.AppCompatActivity;
 
 import java.util.ArrayList;
 import java.util.Collections;
-import java.util.List;
 
 
 public class MainActivity extends AppCompatActivity implements SensorEventListener {
 
-    private SensorManager mSensorManager;
-    private Sensor mSensor;
+    private SensorManager sensorManager;
+    private Sensor temperature;
+    private Sensor humidity;
+    private Sensor pressure;
     private int listLength = 30;
+    double constant = 216.7;
     public ArrayList<Double> list = new ArrayList<Double>();
+    public ArrayList<Double> listH = new ArrayList<Double>();
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-
-
-
-        mSensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
-        mSensor = mSensorManager.getDefaultSensor(Sensor.TYPE_PRESSURE);
-        if (mSensorManager.getDefaultSensor(Sensor.TYPE_PRESSURE) != null){
-            List<Sensor> pressure = mSensorManager.getSensorList(Sensor.TYPE_PRESSURE);
-            System.out.println("There is an air pressure");
-        }
-        else {
-            System.out.println("There is no gyroscope");
-        }
-    }
-
-
-
-    @Override
-    public final void onAccuracyChanged(Sensor sensor, int accuracy) {
-        // Do something here if sensor accuracy changes.
+        setContentView(R.layout.activity_main);// Get an instance of the sensor service, and use that to get an instance of
+        // a particular sensor.
+        sensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
+        temperature = sensorManager.getDefaultSensor(Sensor.TYPE_AMBIENT_TEMPERATURE);
+        humidity = sensorManager.getDefaultSensor(Sensor.TYPE_RELATIVE_HUMIDITY);
+        pressure = sensorManager.getDefaultSensor(Sensor.TYPE_PRESSURE);
     }
 
     @Override
-    public final void onSensorChanged(SensorEvent event) {
-        final double millibars_of_pressure = event.values[0];
-        if(list.size() <= listLength) {
-            list.add(millibars_of_pressure);
+    public void onSensorChanged(SensorEvent sensorEvent) {
+        Sensor sensor = sensorEvent.sensor;
+        if (sensor.getType() == Sensor.TYPE_AMBIENT_TEMPERATURE) {
+            double x = sensorEvent.values[0];
+            System.out.println("The temp is: " + x);
         }
 
-        if(list.size() == listLength) {
-            System.out.println("The max is: " + Collections.max(list)+ " The min is: " + Collections.min(list));
-            onPause();
+        else if (sensor.getType() == Sensor.TYPE_RELATIVE_HUMIDITY) {
+            double x = sensorEvent.values[0];
+            System.out.println("The humid is: " + x);
+
         }
+
+        else if (sensor.getType() == Sensor.TYPE_PRESSURE) {
+            double x = sensorEvent.values[0];
+            if (list.size() <= 49) {
+                list.add(x);
+            }
+
+            if(list.size() == 49) {
+                sensorManager.unregisterListener(this, pressure);
+                System.out.println("The max is: " + Collections.min(list) + "The min is: " + Collections.min(list));
+            }
+        }
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int i) {
+
     }
 
     @Override
     protected void onResume() {
         // Register a listener for the sensor.
         super.onResume();
-        mSensorManager.registerListener(this, mSensor, SensorManager.SENSOR_DELAY_NORMAL);
+        sensorManager.registerListener(this, humidity, SensorManager.SENSOR_DELAY_NORMAL);
+        sensorManager.registerListener(this, temperature, SensorManager.SENSOR_DELAY_NORMAL);
+        sensorManager.registerListener(this, pressure, SensorManager.SENSOR_DELAY_NORMAL);
     }
+
 
     @Override
     protected void onPause() {
         // Be sure to unregister the sensor when the activity pauses.
         super.onPause();
-        mSensorManager.unregisterListener(this);
+        sensorManager.unregisterListener(this);
     }
 }
